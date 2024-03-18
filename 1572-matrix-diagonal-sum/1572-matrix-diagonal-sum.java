0d648b8fd4c473b47e2e6b8148e7a501c8b66565
PRATIK BHURE <147384272+PSBHURE@users.noreply.github.com>
2024-03-19 00:49:23 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (47.54%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,32 +1,14 @@
-
 class Solution {
     public int diagonalSum(int[][] mat) {
-        
-        // side length
-        int n = mat.length;
-        
-        // mid point index
-        int mid = n / 2;
-        
-        // summation of diagonal element
-        int summation = 0;
-        
-        for( int i = 0 ; i < n ; i++ ){
+        int sum=0,n=mat.length,mid=n/2;
+        for(int i=0;i<n;i++)
+        {
+            sum+=mat[i][i];
+            sum+=mat[i][n-i-1];
             
-            // primary diagonal
-            summation += mat[i][i];
-            
-            // secondary diagonal
-            summation += mat[n-1-i][i];
         }
-        
-        
-        if( n % 2 == 1 ){
-            
-            // remove center element (repeated) on odd side-length case
-            summation -= mat[mid][mid];
-        }
-        
-        return summation;
+        if(n%2==1)
+                sum-=mat[mid][mid];
+        return sum;
     }
 }
