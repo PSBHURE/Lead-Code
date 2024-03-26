703f546f3e413166db4fbf2a17e8000836ed9a63
PRATIK BHURE <147384272+PSBHURE@users.noreply.github.com>
2024-03-26 15:56:36 +0530

    Time: 1 ms (56.78%), Space: 41.6 MB (33.22%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,19 +1,12 @@
 class Solution {
     public String sortSentence(String s) {
-      // Split the input string by space into an array of words
-        String[] words = s.split("\\s+");
-        
-        // Create a new array to hold the words sorted by their last character
-        String[] sortedWords = new String[words.length];
-        
-        // Iterate through the words and extract the last character, then store them in the sorted array
-        for (String word : words) {
-            char lastChar = word.charAt(word.length() - 1);
-            // The last character in the word determines its position in the sorted array
-            sortedWords[lastChar - '1'] = word.substring(0, word.length() - 1);
-        }
-
-        // Join the sorted array of words into a single string
-        return String.join(" ", sortedWords);
+      String[] arr=s.split("\\s+");
+      String[] arr1=new String[arr.length];
+      for(String word:arr)
+      {
+          int no=Integer.parseInt(String.valueOf(word.charAt(word.length()-1)));
+            arr1[no-1]=word.substring(0,word.length()-1);
+      }
+        return String.join(" ",arr1);
     }
 }
