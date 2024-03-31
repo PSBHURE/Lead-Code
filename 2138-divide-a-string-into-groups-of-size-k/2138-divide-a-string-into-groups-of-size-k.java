a3b18584ec693be003a4ea0ab31a1683925e4e28
PRATIK BHURE <147384272+PSBHURE@users.noreply.github.com>
2024-04-01 00:51:11 +0530

    Time: 1 ms (96.45%), Space: 42.1 MB (71.29%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String[] divideString(String s, int k, char fill) {
         StringBuffer temp=new StringBuffer(s);
          StringBuffer no=new StringBuffer();
@@ -15,4 +15,43 @@ class Solution {
         }
         return arr;
     }
+}*/
+
+
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+      int x=0;
+        StringBuffer temp=new StringBuffer(s);
+        if(temp.length()%k!=0)
+        {
+            x=k-temp.length()%k;
+            for(int i=0;i<x;i++)
+            {
+                temp.append(fill);
+            }
+        }
+        String[] arr=new String[temp.length()/k];
+        for(int i=0;i<temp.length();i+=k)
+        {
+            arr[i/k]=temp.substring(i,i+k);
+        }
+        
+        return arr;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
