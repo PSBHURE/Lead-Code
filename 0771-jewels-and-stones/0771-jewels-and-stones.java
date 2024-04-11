49ed8544f24d30972665d95e91b7b56b3dafd2db
PRATIK BHURE <147384272+PSBHURE@users.noreply.github.com>
2024-04-11 15:37:01 +0530

    Time: 1 ms (55.88%), Space: 41.6 MB (69.80%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count=0;
         char[] arr=jewels.toCharArray();
@@ -21,6 +21,29 @@ class Solution {
                 count+=entry.getValue();
                 
         }
+        return count;
+    }
+}*/
+
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        char[] arr=jewels.toCharArray();
+        char[] arr1=stones.toCharArray();
+        
+        Set<Character>set=new HashSet<>();
+        for(char ch1:arr)
+        {
+            set.add(ch1);
+        }
+        
+        for(int i=0;i<stones.length();i++)
+        {
+            if(set.contains(stones.charAt(i)))
+                count++;
+        }
+      
         return count;
     }
 }
